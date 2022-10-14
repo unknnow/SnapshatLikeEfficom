e2595d88e28df7a70c046d91618eabaf4f78b260
Brailly Julien <juju.brailly@orange.fr>
2022-10-14 11:49:40 +0200

    pour la localisation

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -29,9 +29,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: Text(widget.title),
-      ),
+      ),*/
       body: _pages[_selectedIndex],
       /*floatingActionButton: FloatingActionButton(
         onPressed: () {},
